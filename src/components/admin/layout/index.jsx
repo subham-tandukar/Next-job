b024880579d264621a79d19a423de0d162541c20
Subham <subhamtndkr9@gmail.com>
2024-10-31 00:49:22 +0545

    admin layout pushed

--- src/components/admin/layout/index.jsx
+++ src/components/admin/layout/index.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
-export default function AdminLayout() {
+export default function AdminLayout({ children }) {
   return (
-    <main className='flex min-h-screen flex-grow'>
-      <aside className='bottom-0 start-0 z-50 h-full w-[270px] border-e-2 border-gray-100 bg-white 2xl:w-72 fixed hidden xl:block dark:bg-gray-50'>
-
+    <main className="flex min-h-screen flex-grow">
+      <aside className="bottom-0 start-0 z-50 h-full w-[270px] border-e-2 border-gray-100 bg-white 2xl:w-72 fixed hidden xl:block dark:bg-gray-50">
+        sidebar
       </aside>
 
-      <div className='flex w-full flex-col xl:ms-[270px] xl:w-[calc(100%-270px)] 2xl:ms-72 2xl:w-[calc(100%-288px)]'>
-
+      <div className="flex w-full flex-col xl:ms-[270px] xl:w-[calc(100%-270px)] 2xl:ms-72 2xl:w-[calc(100%-288px)]">
+        {children}
       </div>
     </main>
-  )
+  );
 }

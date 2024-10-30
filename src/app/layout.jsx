d794b007a1feb21ae0e85f97602315444c6d7778
Subham <subhamtndkr9@gmail.com>
2024-10-31 00:39:53 +0545

    session provider moved to admin folder

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import SessionProviderWrapper from "@/components/sessionProvider";
+import SessionProviderWrapper from "@/components/admin/sessionProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",

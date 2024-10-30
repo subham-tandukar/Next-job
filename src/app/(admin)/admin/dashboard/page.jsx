717405f0bc2b02cdf438985a06bfc53d23a614ff
subham999 <subham@genesiswtech.com>
2024-10-30 17:18:13 +0545

    admin dashboard updated

--- src/app/(admin)/admin/dashboard/page.jsx
+++ src/app/(admin)/admin/dashboard/page.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+"use client";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 export default function AdminDashboard() {
+  const handleLogout = async () => {
+    await signOut({ callbackUrl: "/login" });
+  };
+
   return (
-    <div>Admin Dashboard</div>
-  )
+    <div>
+      Admin Dashboard
+      <button onClick={handleLogout}>Log out</button>
+    </div>
+  );
 }

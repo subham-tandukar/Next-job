06d9db6f9c1aea6d2bb143eb1ff56468dfa8d329
Subham <subhamtndkr9@gmail.com>
2024-10-31 13:24:33 +0545

    admin layout pushed

--- src/components/admin/layout/index.jsx
+++ src/components/admin/layout/index.jsx
@@ -6,7 +6,7 @@ export default function AdminLayout({ children }) {
     <main className="flex min-h-screen flex-grow">
       <AdminSidebar />
 
-      <div className="flex w-full flex-col xl:ms-[270px] xl:w-[calc(100%-270px)] 2xl:ms-72 2xl:w-[calc(100%-288px)]">
+      <div className="flex w-full flex-col xl:ms-[270px] xl:w-[calc(100%-270px)]">
         {children}
       </div>
     </main>
